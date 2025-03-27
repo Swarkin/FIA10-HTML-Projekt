b542aae0e38787f1eb154e65420bd83090dd5a2f
Swarkin <102416174+Swarkin@users.noreply.github.com>
2025-03-27 09:28:06 +0100

    Eingabevalidation und neue Aufgaben generieren

--- src/game.js
+++ src/game.js
@@ -1,40 +1,70 @@
 $("document").ready(bereit);
 
+let antwort_feld;
+let score = 0;
+
 function bereit() {
-	$("#answer").on("keypress", answer_texteingabe);
+	antwort_feld = $("#antwort");
+	antwort_feld.on("keypress", antwort_eingabefilter);
+	
+	let aufgabe = aufgabe_generieren();
+	aufgabe_anzeigen(aufgabe);
 }
 
-let term = null;
-
-function answer_texteingabe(event) {
+function antwort_eingabefilter(event) {
 	if (event.key === "Enter") {
-		let answer = $("#answer");
-		let nummer = parseInt(answer.val());
-		answer.val(""); // Antwortfeld leeren
-		answer_validieren(nummer)
-	} else if (event.key !== "," && isNaN(event.key)) {
-		// Eingabe ignorieren
+		// Enter gedrückt
+		let nummer = parseInt(antwort_feld.val());
+		antwort_feld.val(""); // Antwortfeld leeren
+		if (antwort_validieren(nummer)) {
+			// Richtige Antwort
+			score++;
+			let aufgabe = aufgabe_generieren();
+			aufgabe_anzeigen(aufgabe);
+		}
+	} else if (event.key === "-" && antwort_feld.val().length !== 0) {
+		// Minus nur am Anfang erlauben
 		event.preventDefault();
+	} else if ([",", "-"].includes(event.key) && !antwort_feld.val().includes(event.key)) {
+		// Komma und Minus nur ein mal erlauben
+		return; // Eingabe zulassen
+	} else if (event.key === "." && !antwort_feld.val().includes(",")) {
+		// Punkt mit Komma austauschen
+		antwort_feld.val(antwort_feld.val() + ",");
+		event.preventDefault();
+	} else if (isNaN(event.key)) {
+		// Eingabe ist keine Zahl
+		event.preventDefault(); // Eingabe ablehnen
 	}
 }
 
-function answer_validieren(antwort) {
-	console.log(antwort);
+function antwort_validieren(antwort) {
+	return true;
+}
+
+function aufgabe_anzeigen(aufgabe) {
+	$("#aufgabe").html(aufgabe.join(" "));
 }
 
 function aufgabe_generieren() {
-	let term = [];
-	let parameter_anzahl = zufalls_zahl(1, 3);
+	let aufgabe = [];
+	let parameter_anzahl = zufalls_zahl(2, 3);
 
-	for (let i=0; i<parameter_anzahl; i++) {
-		term.push(zufalls_zahl(0, 9));
+	for (let i = 0; i < parameter_anzahl; i++) {
+		aufgabe.push(zufalls_zahl(0, 9));
 
 		if (i < parameter_anzahl - 1) {
-			term.push(zufalls_rechenzeichen());
+			let rechenzeichen = zufalls_rechenzeichen();
+
+			// todo: Saubere Teilbarkeit überprüfen, 0 vermeiden
+
+			aufgabe.push(rechenzeichen);
 		}
 	}
 
-	return term;
+	aufgabe.push("=");
+
+	return aufgabe;
 }
 
 function zufalls_zahl(min, max) {
