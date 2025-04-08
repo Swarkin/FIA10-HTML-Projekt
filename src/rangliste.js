6f5cdf2824194e50be0728a9178ada8d8709e351
Swarkin <102416174+Swarkin@users.noreply.github.com>
2025-04-08 11:24:01 +0200

    Rangliste mithilfe HTTP-Anfragen

---
+++ src/rangliste.js
@@ -0,0 +1,39 @@
+$("document").ready(bereit);
+
+const Api = "https://score.swarkin.dev"
+const Schwierigkeit = {
+	Leicht: "easy",
+	Normal: "normal",
+	Schwer: "hard",
+}
+
+let rangliste_tbody;
+
+function bereit() {
+	rangliste_tbody = $("#leaderboard-tbody");
+	anzeigen(Schwierigkeit.Normal);
+}
+
+function anzeigen(schwierigkeit) {
+	$.get(Api+"/score?difficulty="+schwierigkeit)
+		.fail(function() {
+			console.log("fail");
+		})
+		.done(function(result) {
+			console.log("done");
+			rangliste_tbody.empty();
+
+			let names = [];
+			let scores = [];
+
+			result.forEach(eintrag => {
+				names.push(eintrag["name"]);
+				scores.push(eintrag["score"])
+			});
+
+			for (let i = 0; i < names.length; i++) {
+				rangliste_tbody.append("<tr><td>"+names[i]+"</td><td>"+scores[i]+"</td></tr>");
+			}
+		}
+	);
+}
